950fa6f5a0564bf18e42adafcfe8c5455ca3f845
Julienne San Luis <julienne.codes@gmail.com>
2021-08-21 16:42:56 -0400

    cleaned up code - removed comments

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import { ColourInput } from "./components/ColourInput";
 import { ColourOutput } from "./components/ColourOutput";
 
 function App() {
-  // hslColour should look like this {hue: 193, saturation: 72, lightness: 65 }
   const [hslColour, setHSLColour] = useState(null);
-
-  // const hslColour = {hue: 193, saturation: 72, lightness: 65 };
-  // I was just testing that the div and paragraph would display the right colour output
 
   return (
     <div className="App">
