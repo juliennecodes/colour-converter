9b76e40eb3300fef46a1c3bc91289cf421fb7b29
Julienne San Luis <julienne.codes@gmail.com>
2022-10-09 14:42:19 -0400

    add max width of 480px and center contents

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ function App() {
   const [hslColour, setHSLColour] = useState(null);
 
   return (
-    <div className="App text-blue-900">
-      <main className="px-6 py-4 grid cols-1 gap-3">
+    <div className="App text-blue-900 grid cols-1 justify-center">
+      <main className="px-6 py-4 grid cols-1 gap-3 min-w-480">
         <h1 className="text-xs font-semibold uppercase text-blue-900">Colour Converter</h1>
         <ColourInput setHSLColour={setHSLColour}/>
-
         <ColourOutput hslColour={hslColour}/>
-
       </main>
     </div>
   );
