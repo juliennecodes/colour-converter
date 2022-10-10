669ce93fd2c1d1cafbbd8e6bef9404e4d8c9ddb0
Julienne San Luis <julienne.codes@gmail.com>
2022-10-09 21:08:18 -0400

    restructure project to have frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
+ 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+ 
+const port = 8021;
+const app = express();
+const buildPath = path.join(__dirname, 'build');
+ 
+app.use(express.static(buildPath));
+ 
+app.get("/", (req, res) => {
+ res.sendFile(path.join(buildPath, 'index.html'));
+});
+ 
+app.listen(port, () => {
+   console.log(`Listening on port ${port}`);
+});
